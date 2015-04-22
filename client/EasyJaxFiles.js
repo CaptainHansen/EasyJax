f91391d61994df11d41db3117fdb82205a36f067
Stephen Hansen <steve@hansencomputers.net>
2015-04-22 11:11:35 -0700

    Removing the license from the top of each file.  Its annoying me.  Placing full text in LICENSE, referencing the license in each file

--- client/EasyJaxFiles.js
+++ client/EasyJaxFiles.js
@@ -1,33 +1,10 @@
-/*
- * Copyright (c) 2013 Stephen Hansen (www.hansencomputers.com)
- * 
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
-
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE. 
- */
-
 /** EasyJaxFiles Javascript class
  *
  * instantiate this class with the URL to post files to (PHP script using the EasyJaxFiles PHP class)
  * supply a callback function that will run after each successful upload.  If an error is returned,
  * it is announced using the alert() function
+ * 
+ * License: MIT
  */
  
  // make sure we have the sendAsBinary method on all browsers
